11f3e409b8a23f5b9b834b26a205641279edbe6e
viile <zhudejiang@qutoutiao.net>
2020-07-05 20:22:43 +0800

    init

--- gomoku/game.go
+++ gomoku/game.go
@@ -9,3 +9,19 @@ type Game struct {
 	//
 	currPoint common.Pos
 }
+
+func NewGame() *Game {
+	g := &Game{
+		G:common.NewG(15,15),
+	}
+	return g
+}
+
+func (g *Game) winCheckPoint(p common.Pos) (bool) {
+	return false
+}
+
+
+func (g *Game) winCheck(p common.Pos,v int) (bool) {
+	return g.Each(g.winCheckPoint)
+}
