94e0c3b100fd0dab0fe7a71757338c0bd0b09a47
viile <zhudejiang@qutoutiao.net>
2020-07-16 16:48:49 +0800

    init

--- gomoku/game_test.go
+++ gomoku/game_test.go
@@ -1,10 +1,19 @@
 package gomoku
 
 import (
+	"github.com/magiconair/properties/assert"
+	"github.com/viile/games/common"
 	"testing"
 )
 
 func TestGameWin(t *testing.T) {
-	//g := NewGame
+	g := NewGame()
+	g.Set(common.Pos{0,1},PointBlack{})
+	g.Set(common.Pos{0,2},PointBlack{})
+	g.Set(common.Pos{0,3},PointBlack{})
+	g.Set(common.Pos{0,4},PointBlack{})
+	g.Set(common.Pos{0,5},PointBlack{})
 
+	assert.Equal(t,g.win(PointBlackValue),true,"black win")
+	assert.Equal(t,g.win(PointWhiteValue),false,"white lose")
 }
